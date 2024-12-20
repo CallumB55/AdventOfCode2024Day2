012e14a0b53d3c94854f5a77dbc5f201d9f40e54
callum <callum.binns@insitetrack.com>
2024-12-20 19:52:25 +0000

    initial commit

---
+++ src/main/java/com/callumhobby/adventofcode2024day2/AdventOfCode2024Day2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.callumhobby.adventofcode2024day2;
+
+/**
+ *
+ * @author CallumBinns
+ */
+public class AdventOfCode2024Day2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
